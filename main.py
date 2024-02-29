2ffb7cfb639a2d4fb36751cbeddcf6a01cd708f4
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2024-02-29 21:21:36 +0300

    output parsing

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+from typing import List, Union
 from dotenv import load_dotenv
+from langchain.tools import Tool
 from langchain.agents import tool
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.tools.render import render_text_description
+from langchain_core.agents import AgentAction, AgentFinish
 from langchain.agents.output_parsers import ReActSingleInputOutputParser
 
 
 load_dotenv()
+
+
+def find_tool_by_name(tools: List[Tool], name: str) -> Tool:
+    for tool in tools:
+        if tool.name == name:
+            return tool
+    raise ValueError(f"Tool with name {name} not found")
 
 
 @tool
@@ -45,10 +55,19 @@ if __name__ == "__main__":
         tool_names=", ".join([t.name for t in tools]),
     )
 
-    llm = ChatOpenAI(temperature=0)
+    llm = ChatOpenAI(temperature=0, model_kwargs={"stop": ["\nObservation"]})
     agent = (
         {"input": lambda x: x["input"]} | prompt | llm | ReActSingleInputOutputParser()
     )
 
-    res = agent.invoke({"input": "What is the length 'DOG' in characters?"})
-    print(res)
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+        {"input": "What is the length DOG in characters?"}
+    )
+
+    if isinstance(agent_step, AgentAction):
+        tool_name = agent_step.tool
+        tool_to_use = find_tool_by_name(tools, tool_name)
+        tool_input = agent_step.tool_input
+
+        observation = tool_to_use.func(str(tool_input))
+        print(f"{observation}")
