c7fecaac0abdfe303939d1de0c3cb5044e8e4e9c
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2024-02-29 20:45:23 +0300

    create reAct prompt

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from dotenv import load_dotenv
 from langchain.agents import tool
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.tools.render import render_text_description
+
 
 load_dotenv()
 
@@ -12,3 +16,32 @@ def get_text_length(text: str) -> int:
 
 if __name__ == "__main__":
     tools = [get_text_length]
+
+    template = """
+        Answer the following questions as best you can. You have access to the following tools:
+
+        {tools}
+
+        Use the following format:
+
+        Question: the input question you must answer
+        Thought: you should always think about what to do
+        Action: the action to take, should be one of [{tool_names}]
+        Action Input: the input to the action
+        Observation: the result of the action
+        ... (this Thought/Action/Action Input/Observation can repeat N times)
+        Thought: I now know the final answer
+        Final Answer: the final answer to the original input question
+
+        Begin!
+
+        Question: {input}
+        Thought:
+    """
+
+    prompt = PromptTemplate.from_template(template=template).partial(
+        tools=render_text_description(tools),
+        tool_names=", ".join([t.__name__ for t in tools]),
+    )
+
+    llm = ChatOpenAI(temperature=0, stop=["\nObservation"])
